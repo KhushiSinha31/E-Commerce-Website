0aad80f897045dfc99d450298e65410ca25e9584
Khushi <khushisinha144@gmail.com>
2021-07-29 11:43:45 +0530

    Inital commit

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Online Shopping. All Rights Reserved |  Contact Us: +91 9523760000</p>
+				<p>Credit:
+				<p>Khushi Sinha </p>
+			<div class="social-icons" data-reactid=".4gad7jixxk.3.0.1.0.2">
+					<div class="footer-svg-social-icons" data-reactid=".4gad7jixxk.3.0.1.0.2.0.0.0">
+						<a target="_blank" class="" href="https://www.linkedin.com/in/khushi-sinha-677005215" data-reactid=".4gad7jixxk.3.0.1.0.2.0.0.0.0">
+						<img src="https://img.icons8.com/ios-filled/100/ffffff/linkedin.png" data-reactid=".4gad7jixxk.3.0.1.0.2.0.0.0.0.0" width="20" height="20">
+						</a>
+					</div>
+					<div class="footer-svg-social-icons" data-reactid=".4gad7jixxk.3.0.1.0.2.0.1.0">
+						<a target="_blank" class="" href="https://www.facebook.com/" data-reactid=".4gad7jixxk.3.0.1.0.2.0.1.0.0">
+								<img src="//images11.voonik.com/web_images/facebook.png" data-reactid=".4gad7jixxk.3.0.1.0.2.0.1.0.0.0" width="20" height="20">
+						</a>
+					</div>
+					<div class="footer-svg-social-icons" data-reactid=".4gad7jixxk.3.0.1.0.2.0.2.0">
+							<a target="_blank" class="" href="https://www.instagram.com/khushisinha31/" data-reactid=".4gad7jixxk.3.0.1.0.2.0.2.0.0" >
+							<img src="//images11.voonik.com/web_images/instagram-logo.png" data-reactid=".4gad7jixxk.3.0.1.0.2.0.2.0.0.0" width="20" height="20">
+							</a>
+						</div>
+					<div class="footer-svg-social-icons" data-reactid=".4gad7jixxk.3.0.1.0.2.0.3.0">
+						<a target="_blank" class="" href="https://www.github.com/khushisinha31/" data-reactid=".4gad7jixxk.3.0.1.0.2.0.3.0.0">
+							<img src="https://img.icons8.com/ios-filled/60/ffffff/github.png" data-reactid=".4gad7jixxk.3.0.1.0.2.0.3.0.0.0" width="20" height="20">
+						</a>
+					</div>
+			</div>
+        </center>
+    </div>
+</footer>
